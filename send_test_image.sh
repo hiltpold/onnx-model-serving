7d854ef12e083d13ac97b1671b9557ef7c095222
hiltpold <m.hiltpold@protonmail.ch>
2023-12-21 11:57:33 +0100

    minor changes

--- send_test_image.sh
+++ send_test_image.sh
@@ -7,6 +7,7 @@ then
 	exit 1
 fi
 # detect military aircrafts in the base64 encoded image
-RESULT=$((echo -n '{"image_format": "jpg", "model_name":"militaryaircrafts.onnx", "image": "'; base64 ./test/test.jpg; echo '"}') | curl -H "Content-Type: application/json" -d @-  http://localhost:5000/api/v1/objectdetection)
+RESULT=$((echo -n '{"image_format": "jpg", "model_name":"militaryaircrafts.onnx", "image": "'; base64 ./test/testtest.jpg; echo '"}') | curl -H "Content-Type: application/json" -d @-  http://localhost:5000/api/v1/objectdetection)
+echo $RESULT
 RESULT_IMAGE=$(echo $RESULT | jq '.result_image')
 echo $RESULT_IMAGE | base64 -d --ignore-garbage > ./test/pred/test.jpg
